fc1998bd706e704236a4f89aab584b24dbe2be48
Ema Bícová <104419354+scieLabs@users.noreply.github.com>
2025-03-26 11:42:47 +0100

    Revert "navigation"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import HomePage from './pages/HomePage';
-import CreatePost from './pages/CreatePost';
-import PostDetails from './pages/PostDetails';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <Router>
-        <Routes>
-          <Route path='/' element={<MainLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path='create' element={<CreatePost />} />
-            <Route path='post/:id' element={<PostDetails />} />
-          </Route>
-        </Routes>
-    </Router>
-  )
+    <div className="appContainer">
+      <h1 className="text-2xl">Fullstack DnD</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
